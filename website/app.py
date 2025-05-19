8c17406a32bb80d2c3dd413cb248a884fbe8ddf2
Kushal patil <kushalpatil@laptopbqhy.mynetworksettings.com>
2025-05-19 16:53:28 -0400

    Stockfish is working and playing moves now...

--- website/app.py
+++ website/app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, request, url_for, redirect
+import mimetypes
 
+mimetypes.add_type('application/wasm', '.wasm')
 app = Flask(__name__)
+@app.after_request
+def add_coop_coep_headers(response):
+    response.headers['Cross-Origin-Opener-Policy'] = 'same-origin'
+    response.headers['Cross-Origin-Embedder-Policy'] = 'require-corp'
+    return response
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     print("yo")
